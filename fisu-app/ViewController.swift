10040cf2ed584c00e5da01495adf193b899569a4
pierre-louis faure <pierre-louis.faure@igtp4-mac06.isim.intra>
2016-02-04 12:52:20 +0100

    Initial Commit

---
+++ fisu-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fisu-app
+//
+//  Created by pierre-louis faure on 04/02/2016.
+//  Copyright © 2016 Faure Geoffray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
