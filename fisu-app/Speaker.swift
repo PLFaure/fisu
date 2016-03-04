ef54922391234854ce6fae59bfed714090af5be2
IG2013 <ig2013@igtp4-mac01-3.local>
2016-03-04 16:23:04 +0100

    ActivityType, Location, Speaker, Event, EventsSet & User classes creation

---
+++ fisu-app/Speaker.swift
@@ -0,0 +1,13 @@
+//
+//  Speaker.swift
+//  fisu-app
+//
+//  Created by IG2013 on 04/03/2016.
+//  Copyright © 2016 Faure Geoffray. All rights reserved.
+//
+
+import UIKit
+
+class Speaker: NSObject {
+
+}
