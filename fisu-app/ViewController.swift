24d1642497587f2f24cdd6a0d65a5f16a77e9207
IG2013 <ig2013@igtp4-mac01-3.local>
2016-03-17 18:35:54 +0100

    data base infos (2)

--- fisu-app/ViewController.swift
+++ fisu-app/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController {
     // Retreive the managedObjectContext from AppDelegate
     let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     
+    //Creation of all datas
     var events = [Event]() // Create an empty array of Events
     var activityTypes = [ActivityType]() // Create an empty array of ActivityTypes
     var locations = [Location]() // Create an empty array of Locations
-
+    var accomodations = [Accomodation]()
+    var restaurants = [Restaurant]()
+    var speakers = [Speaker]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,8 +44,10 @@ class ViewController: UIViewController {
         
         // Create locations data
         let l = [
-            (("43.593333"),("3.849431"),("Stade de l'assiette")),
-            (("43.528198"),("3.934645"),("La Plage"))
+            ("43.593333", "3.849431", "Stade de l'assiette"),
+            ("43.528198", "3.934645", "La Plage"),
+            ("43.632699", "3.862738", "Polyetch"),
+            ("43.631113", "3.860338", "2 Place Eugène Bataillon")
         ]
         // Loop through, creating locations
         for (lLatitude, lLongitude, lName) in l {
@@ -63,19 +68,50 @@ class ViewController: UIViewController {
             Event.createInManagedObjectContext(moc, name: eventName, date: eventDate, descr: eventDescr, type: eventType, loc: eventLoc, speakers: eventSpeakers)
         }
         fetchEvent()
+        
+        // Create accomodations data
+        let a = [
+            ("Booba Room","Une salle trop fat. En plus il y a une cafetiere.", NSData(),locations[2])
+        ]
+        //Loop through, creating accomodations
+        for (aName, aDescr, aPict, aLoc) in a {
+            // Create an individual accomodation
+            Accomodation.createInManagedObjectContext(moc, name: aName, descr: aDescr, picture: aPict, loc: aLoc)
+        }
+        fetchAccomodation()
+        
+        // Create restaurants data
+        let r = [
+            ("Le RU","N'y vas pas, c'est pas bon...", NSData(),locations[3])
+        ]
+        //Loop through, creating restaurants
+        for (rName, rDescr, rPict, rLoc) in r {
+            // Create an individual restaurant
+            Restaurant.createInManagedObjectContext(moc, name: rName, descr: rDescr, picture: rPict, loc: rLoc)
+        }
+        fetchRestaurant()
+        
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////                ICI              ///////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // Create speakers data
+        let sp = [
+            ("Kenobi", "Obi Wan", "Male", "Jedi Master", "The Force is strong with this one.", "obi.keke@jeditemple.cor", "06 00 00 00 00", "75 rue du ")
+        ]
+        //Loop through, creating accomodations
+        //lastName: String, firstName: String, sex: String, title: String, descr: String, email: String, phone:String, address: String, picture: NSData
+        for (rName, rDescr, rPict, rLoc) in r {
+            // Create an individual accomodation
+            Restaurant.createInManagedObjectContext(moc, name: rName, descr: rDescr, picture: rPict, loc: rLoc)
+        }
+        fetchSpeaker()
+        //////////////////////////////////////////////////////////////////////////////////////
+        
         save()
     }
     
     func fetchActivityType() {
         let fetchRequest = NSFetchRequest(entityName: "ActivityType")
-        
-        // Create a sort descriptor object that sorts on the "label"
-        // property of the Core Data object
-        let sortDescriptor = NSSortDescriptor(key: "label", ascending: true)
-        
-        // Set the list of sort descriptors in the fetch request,
-        // so it includes the sort descriptor
-        fetchRequest.sortDescriptors = [sortDescriptor]
         
         do {
             if let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest) as? [ActivityType] {
@@ -90,14 +126,6 @@ class ViewController: UIViewController {
     
     func fetchLocation() {
         let fetchRequest = NSFetchRequest(entityName: "Location")
-        
-        // Create a sort descriptor object that sorts on the "label"
-        // property of the Core Data object
-        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
-        
-        // Set the list of sort descriptors in the fetch request,
-        // so it includes the sort descriptor
-        fetchRequest.sortDescriptors = [sortDescriptor]
         
         do {
             if let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest) as? [Location] {
@@ -130,6 +158,34 @@ class ViewController: UIViewController {
             NSLog("Fetch failed: \(nserror.localizedDescription)")
         }
 
+    }
+    
+    func fetchAccomodation() {
+        let fetchRequest = NSFetchRequest(entityName: "Accomodation")
+        
+        do {
+            if let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest) as? [Accomodation] {
+                accomodations = fetchResults
+            }
+        } catch {
+            let nserror = error as NSError
+            NSLog("Fetch failed: \(nserror.localizedDescription)")
+        }
+        
+    }
+    
+    func fetchRestaurant() {
+        let fetchRequest = NSFetchRequest(entityName: "Restaurant")
+        
+        do {
+            if let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest) as? [Restaurant] {
+                restaurants = fetchResults
+            }
+        } catch {
+            let nserror = error as NSError
+            NSLog("Fetch failed: \(nserror.localizedDescription)")
+        }
+        
     }
     
     func save() {
