0c61136eda0cc8cd28255e30998791387d622740
IG2013 <ig2013@igtp4-mac01-3.local>
2016-03-24 16:25:10 +0100

    event tests

--- fisu-app/User+CoreDataProperties.swift
+++ fisu-app/User+CoreDataProperties.swift
@@ -22,6 +22,6 @@ extension User {
     @NSManaged var phone: String?
     @NSManaged var sex: String?
     @NSManaged var userName: String?
-    @NSManaged var event: EventsSet?
+    @NSManaged var event: NSSet?
 
 }
