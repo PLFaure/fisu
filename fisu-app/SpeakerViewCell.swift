fc360be26bd12a0ba84a95fc29c2504ddd5b6484
IG2013 <ig2013@igtp4-mac01-6.local>
2016-04-04 13:42:23 +0200

    speakers

---
+++ fisu-app/SpeakerViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SpeakerViewCell.swift
+//  fisu-app
+//
+//  Created by IG2013 on 04/04/2016.
+//  Copyright © 2016 Faure Geoffray. All rights reserved.
+//
+
+import UIKit
+
+class SpeakerViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameSpeaker: UILabel!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
